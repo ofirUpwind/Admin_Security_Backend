1f2a977766320284a62809d5fd05700403b57403
ofirUpwind <148869301+ofirUpwind@users.noreply.github.com>
2023-11-26 10:06:06 +0200

    update config

--- website/config.py
+++ website/config.py
@@ -3,13 +3,15 @@ from sqlalchemy import create_engine
 from dotenv import load_dotenv
 load_dotenv()
 
+
 class Config(object):
 
     # Fetch the local and production database URIs from environment variables
     SQLALCHEMY_DATABASE_URI_LOCAL = os.environ.get('DATABASE_URI_LOCAL')
     SQLALCHEMY_DATABASE_URI_PROD = os.environ.get('DATABASE_URI_PROD')
+
     # Determine which URI to use
-    if os.getenv('IS_NOT_LOCALHOST'):
+    if os.getenv('IS_LOCALHOST') == 'false':
         # Use the production database URI if not localhost
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI_PROD
     else:
